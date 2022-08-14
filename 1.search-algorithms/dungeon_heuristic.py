38dd6f71fc5516e1b4136cac8885605e7bbc2312
HannahHatem <65696094+HannahHatem@users.noreply.github.com>
2022-08-14 18:53:55 +0200

    Update dungeon_heuristic.py

--- 1.search-algorithms/dungeon_heuristic.py
+++ 1.search-algorithms/dungeon_heuristic.py
@@ -11,12 +11,6 @@ def weak_heuristic(problem: DungeonProblem, state: DungeonState):
 
 
 def strong_heuristic(problem: DungeonProblem, state: DungeonState) -> float:
-    #TODO: ADD YOUR CODE HERE
-    #IMPORTANT: DO NOT USE "problem.is_goal" HERE.
-    # Calling it here will mess up the tracking of the explored nodes count
-    # which is considered the number of is_goal calls during the search
-    #NOTE: you can use problem.cache() to get a dictionary in which you can store information that will persist between calls of this function
-    # This could be useful if you want to store the results heavy computations that can be cached and used across multiple calls of this function
     coins = state.remaining_coins
     maxDist = 0
     exitDist = manhattan_distance(state.player, problem.layout.exit)
